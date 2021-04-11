da18f73ea5e904ae368ba6014822ba1e53960162
Your Name <you@example.com>
2021-04-12 01:51:21 +0700

    fixed post form to dbs

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 // var logger = require("morgan");
 var session = require("express-session");
-var multer = require("multer");
+// var multer = require("multer");
 var moment = require("moment");
 var expressValidator = require("express-Validator");
 var mongodb = require("mongodb");
 var mongoose = require("mongoose");
 
-var db = require('monk')('localhost/nodeblog');
+var db = require("monk")("localhost/nodeblog");
+// var upload = multer({ dest: 'uploads/' });
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var postsRouter = require("./routes/posts");
 
 var app = express();
+
+
+app.locals.moment = moment;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -25,14 +29,16 @@ app.set("view engine", "pug");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
+app.use(
+	session({
+		secret: "secret",
+		saveUninitialized: true,
+		resave: true,
+	})
+);
 
 // // Express Validator
 // app.use(expressValidator({
@@ -52,7 +58,6 @@ app.use(session({
 //   }
 // }));
 
-
 // connect-flash
 // app.use(require("connect-flash"));
 // app.use((req, res, next) => {
@@ -66,7 +71,7 @@ app.use(session({
 // });
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -85,7 +90,7 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(3000, () => {
-	console.log(`Example app listening at http://localhost:3000`)
-  })
+	console.log(`Example app listening at http://localhost:3000`);
+});
 
 module.exports = app;
